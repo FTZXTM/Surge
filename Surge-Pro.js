fbcd78eaa83f640d826905888a598d5b91fada60
FTZXTM <106225521+FTZXTM@users.noreply.github.com>
2022-08-24 21:19:46 +0800

    Update Surge-Pro.js

--- Surge-Pro.js
+++ Surge-Pro.js
@@ -4,26 +4,28 @@
  * 由@Rabbit-Spec修改
  * 更新日期：2022.06.15
  * 版本：1.5
-可选参数：授权有效期，后面填入你功能订阅到期的时间。
-----------------------------------------
 */
+
 let params = getParams($argument)
+
 !(async () => {
 /* 时间获取 */
 let traffic = (await httpAPI("/v1/traffic","GET"))
 let dateNow = new Date()
 let dateTime = Math.floor(traffic.startTime*1000)
 let startTime = timeTransform(dateNow,dateTime)
+
 if ($trigger == "button") await httpAPI("/v1/profiles/reload");
+
   $done({
       title:"Surge Pro®",
-      content:
-	`启动时长: ${startTime}\n` +
-        `订阅到期: 2022-06-03`,
+      content:`启动时长: ${startTime}`,
 		icon: params.icon,
 		"icon-color":params.color
-    })
+    });
+
 })();
+
 function timeTransform(dateNow,dateTime) {
 let dateDiff = dateNow - dateTime;
 let days = Math.floor(dateDiff / (24 * 3600 * 1000));//计算出相差天数
@@ -35,7 +37,9 @@ let minutes=Math.floor(leave2/(60*1000))//计算相差分钟数
 //计算相差秒数
 let leave3=leave2%(60*1000)      //计算分钟数后剩余的毫秒数
 let seconds=Math.round(leave3/1000)
+
 if(days==0){
+
 	if(hours==0){
 	if(minutes==0)return(`${seconds}秒`);
 	return(`${minutes}分${seconds}秒`)
@@ -44,7 +48,10 @@ if(days==0){
 	}else {
 	return(`${days}天${hours}时${minutes}分`)
 	}
+
 }
+
+
 function httpAPI(path = "", method = "POST", body = null) {
     return new Promise((resolve) => {
         $httpAPI(method, path, body, (result) => {
@@ -52,6 +59,7 @@ function httpAPI(path = "", method = "POST", body = null) {
         });
     });
 }
+
 function getParams(param) {
   return Object.fromEntries(
     $argument
